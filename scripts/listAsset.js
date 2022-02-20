6879afee245b76e5d2107d3030df7362c75bf75e
Che Coelho <checoelho@gmail.com>
2022-02-20 15:36:20 +0200

    List asset logging

--- scripts/listAsset.js
+++ scripts/listAsset.js
@@ -1,6 +1,11 @@
 const { ethers } = require('hardhat')
 var ExchangeDeploymentData = require('../instance/Exchange.json')
 
+//Set Listing Params:
+const _tokenId = 0 //Index of the mapping where the token addresses are stored
+const _allownace = 100 //Number of tokens to be listed
+const _pricePerToken = 3 //Price in wei for a single token
+
 async function main() {
   const ExchangeCurrentDeployment = ExchangeDeploymentData.currentDeployment
 
@@ -8,10 +13,21 @@ async function main() {
 
   const exchangeContract = Exchange.attach(ExchangeCurrentDeployment)
   try {
-    test = await exchangeContract.listAsset(0, 100, 1)
-    console.log(test)
-    let listing = await exchangeContract.getListing(0)
-    console.log(listing)
+    let lastListing = await exchangeContract.totalListings()
+
+    let listingTx = await exchangeContract.listAsset(
+      _tokenId,
+      _allownace,
+      _pricePerToken
+    )
+    const listing = await exchangeContract.getListing(parseInt(lastListing))
+    console.log(`Listing successfully registered on the exchange: \n 
+    Token Address: ${listing[0]} \n
+    Listed By: ${listing[1]} \n
+    Total Tokens Listed: ${listing[2]} \n
+    Avalaible Tokens: ${listing[3]} \n
+    Price Per Token: ${listing[4]} \n
+    Emptied: ${listing[5]} \n`)
   } catch (error) {
     console.error(error)
   }
