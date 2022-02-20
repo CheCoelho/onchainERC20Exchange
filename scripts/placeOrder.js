101c1664892d2179c56df3acf8f8a3a461e28ab4
Che Coelho <checoelho@gmail.com>
2022-02-20 15:36:33 +0200

    Place order logging

--- scripts/placeOrder.js
+++ scripts/placeOrder.js
@@ -1,25 +1,40 @@
 const { ethers } = require('hardhat')
-var TokenDeploymentData = require('../instance/Token.json')
 var ExchangeDeploymentData = require('../instance/Exchange.json')
 const ExchangeCurrentDeployment = ExchangeDeploymentData.currentDeployment
-require('dotenv').config()
 
-privateKey2 = process.env.PRIVATE_KEY2
+//Set Order Params:
+const _tokenId = 0 //Index of the mapping where the token addresses are stored
+const _amount = 10 //Number of tokens to be listed
+const _pricePerToken = 3 //Price in wei for a single token
+const address =
+  '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d' //Address to place order
 
 const provider = new ethers.providers.JsonRpcProvider() // using default http://localhost:8545
-const signer = new ethers.Wallet(
-  '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',
-  provider
-)
+const signer = new ethers.Wallet(address, provider)
 async function main() {
-  const myContract = await ethers.getContractAt(
+  const exchangeContract = await ethers.getContractAt(
     'Exchange',
     ExchangeCurrentDeployment,
     signer
   )
-  await myContract.placeOrder(0, 10, { value: ethers.utils.parseEther('0.01') })
-  const out = await myContract.getOrder(1)
-  console.log(out)
+  let lastOrder = await exchangeContract.totalOrders()
+
+  let orderTx = await exchangeContract.placeOrder(_tokenId, _amount, {
+    value: _amount * _pricePerToken,
+  })
+  try {
+    const order = await exchangeContract.getOrder(lastOrder)
+
+    console.log(`Order successfully placed on the exchange: \n 
+    Token Address: ${order[0]} \n
+    Ordered By: ${order[1]} \n
+    Total Tokens Fulfilled: ${order[2]} \n
+    Total Tokens Ordered: ${order[3]} \n
+    Total Funding: ${order[4]} \n
+    Closed: ${order[5]} \n`)
+  } catch (error) {
+    console.error(error)
+  }
 }
 main()
   .then(() => process.exit(0))
