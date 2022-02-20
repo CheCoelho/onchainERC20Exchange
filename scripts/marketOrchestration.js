44e10c5c9945e56a10a6cc28e994061bc8e3ce96
Che Coelho <checoelho@gmail.com>
2022-02-20 16:23:04 +0200

    Execute Trade

--- scripts/marketOrchestration.js
+++ scripts/marketOrchestration.js
@@ -1,39 +1,78 @@
 const { ethers } = require('hardhat')
-var TokenDeploymentData = require('../instance/Token.json')
 var ExchangeDeploymentData = require('../instance/Exchange.json')
-const ExchangeCurrentDeployment = ExchangeDeploymentData.currentDeployment
-require('dotenv').config()
+var TokenDeploymentData = require('../instance/Token.json')
 
-privateKey2 = process.env.PRIVATE_KEY2
+const ExchangeCurrentDeployment = ExchangeDeploymentData.currentDeployment
+const TokenCurrentDeployment = TokenDeploymentData.currentDeployment
+const accountTwoPrivateKey =
+  '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d'
 
 const provider = new ethers.providers.JsonRpcProvider() // using default http://localhost:8545
-const signer = new ethers.Wallet(
-  '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',
-  provider
-)
+const signer = new ethers.Wallet(accountTwoPrivateKey, provider)
 async function main() {
-  const myContract = await ethers.getContractAt(
+  const exchangeContract = await ethers.getContractAt(
     'Exchange',
     ExchangeCurrentDeployment,
     signer
   )
+  const tokenContract = await ethers.getContractAt(
+    'Token',
+    TokenCurrentDeployment,
+    signer
+  )
 
-  let orderCount = parseInt(await myContract.totalOrders())
+  let orderCount = parseInt(await exchangeContract.totalOrders())
 
   console.log(`Found ${orderCount} orders.`)
   let orders = []
   for (let i = 0; i < orderCount; i++) {
-    let foundOrder = await myContract.getOrder(i)
+    let foundOrder = await exchangeContract.getOrder(i)
     orders.push(foundOrder)
   }
 
-  let listingCount = parseInt(await myContract.totalListings())
+  let listingCount = parseInt(await exchangeContract.totalListings())
 
   console.log(`Found ${listingCount} listings.`)
   let listings = []
   for (let i = 0; i < listingCount; i++) {
-    let foundListing = await myContract.getListing(i)
+    let foundListing = await exchangeContract.getListing(i)
     listings.push(foundListing)
+  }
+
+  for (let orderIndex in orders) {
+    let order = orders[orderIndex]
+    if (order['closed']) {
+      continue
+    }
+    for (let listingIndex in listings) {
+      let listing = listings[listingIndex]
+      if (listing['emptied']) {
+        continue
+      }
+      if (order['token'] === listing['token']) {
+        if (order['funding'] >= listing['pricePerToken'] * order['amount']) {
+          console.log('Executing trade...')
+          await exchangeContract.triggerOrderFullfillment(
+            orderIndex,
+            listingIndex,
+            order['amount'],
+            order['token']
+          )
+        }
+        let updatedOrderAgentTokens = await tokenContract.balanceOf(order[1])
+        let tokenName = await tokenContract.name()
+        let tokenSymbol = await tokenContract.symbol()
+
+        console.log(
+          `Agent who placed order now has ${updatedOrderAgentTokens} ${tokenName}(${tokenSymbol}) tokens.`
+        )
+        console.log(
+          `The listing agent received ${
+            listing['pricePerToken'] * order['amount']
+          } wei for ${order['amount']} ${tokenName}(${tokenSymbol}) tokens.`
+        )
+      }
+    }
   }
 }
 main()
